986a83b0c8364e3ce0f8b618caed6f3209a53fd1
Damian <damian.ogorek.1996@gmail.com>
2021-08-15 22:51:23 +0200

    Updated widget by adding expanded option list;

--- lib/screens/products_overview_scren.dart
+++ lib/screens/products_overview_scren.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/widgets/products_grid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only favorites'),
+                value: FilterOption.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show all'),
+                value: FilterOption.All,
+              ),
+            ],
+            icon: Icon(Icons.more_vert),
+            onSelected: (FilterOption selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOption.Favorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+          )
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
 
-
+enum FilterOption { Favorites, All }
