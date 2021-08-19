21069db637f164b5a232f7a9eda67c8090984eed
Damian <damian.ogorek.1996@gmail.com>
2021-08-19 21:33:55 +0200

    Created method which return total amount of added items into shop cart;

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -50,4 +50,12 @@ class Cart with ChangeNotifier {
   int get itemCount {
     return _items.length;
   }
+
+  double get totalAmount {
+    double total = 0.0;
+    _items.forEach((key, value) {
+      total += value.price * value.quantity;
+    });
+    return total;
+  }
 }
