882131418ff60e2953969a364a99a36f5fe2fbaa
Damian <damian.ogorek.1996@gmail.com>
2021-08-22 21:05:22 +0200

    Created route name; Updated widget by adding app drawer;

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/providers/orders.dart' show Orders;
+import 'package:flutter_complete_guide/widgets/app_drawer.dart';
 import '/widgets/order_item.dart';
 import 'package:provider/provider.dart';
 
 class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
+
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<Orders>(context);
@@ -12,6 +15,7 @@ class OrdersScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Your orders'),
       ),
+      drawer: AppDrawer(),
       body: ListView.builder(
         itemBuilder: (context, index) => OrderItem(
           orderData.orders[index],
