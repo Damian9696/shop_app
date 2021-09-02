aabc553c97a98e646d5ebb92f5fa0421b864d930
Damian <damian.ogorek.1996@gmail.com>
2021-09-02 21:58:06 +0200

    Created function to add new product;

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -48,4 +48,16 @@ class Products with ChangeNotifier {
   Product findById(String id) {
     return _items.firstWhere((element) => element.id == id);
   }
+
+  void addProduct(Product product) {
+    final Product newProduct = Product(
+      id: DateTime.now().toString(),
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+    );
+    _items.add(newProduct);
+    notifyListeners();
+  }
 }
